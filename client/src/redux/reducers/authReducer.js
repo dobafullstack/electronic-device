a248ce63283fff234df6361e84313c00dde961e9
doba <doba.fullstack@gmail.com>
2022-01-12 01:37:18 +0700

    Fix production baseURL

--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const __prod__ = process.env.NODE_ENV && process.env.NODE_ENV === "development";
+const __prod__ = process.env.NODE_ENV && process.env.NODE_ENV === "production";
 
 const baseUrl = __prod__ ? process.env.REACT_APP_BASE_URL : 'http://localhost:4000';
 
